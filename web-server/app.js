f68cc0ec3820b1f164450db07ba1d7ca182abbec
Xuanzhi Zhang <zxzsaga@gmail.com>
2014-10-11 18:15:30 +0800

    first commit

---
+++ web-server/app.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var app = express.createServer();
+
+app.configure(function(){
+    app.use(express.methodOverride());
+    app.use(express.cookieParser('secret'));
+    app.use(express.session());
+    app.use(express.bodyParser());
+    app.use(app.router);
+    app.set('view engine', 'jade');
+    app.set('views', __dirname + '/public');
+    app.set('view options', {layout: false});
+    app.set('basepath',__dirname + '/public');
+});
+
+app.configure('development', function(){
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+    var oneYear = 31557600000;
+    app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
+    app.use(express.errorHandler());
+});
+
+console.log("Web server has started.\nPlease log on http://127.0.0.1:3001/index.html");
+
+app.listen(3001);
+
+
+// routers
+app.get('/', function(req, res) {
+    if (req.session.accessId) {
+        res.render('main');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+app.get('/login', function(req, res) {
+    res.render('login');
+});
+
+app.get('/register', function(req, res) {
+    res.render('register');
+});
