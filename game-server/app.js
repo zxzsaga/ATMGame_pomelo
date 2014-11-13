dda7dc7702bb4d25a5101d9fd00dcfaf6760a76e
Xuanzhi Zhang <zxzsaga@gmail.com>
2014-11-13 20:21:10 +0800

    go home, save codes

--- game-server/app.js
+++ game-server/app.js
@@ -1,13 +1,16 @@
 var pomelo = require('pomelo');
 var app = pomelo.createApp();
 
-var fs = require('fs');
 var util = require('util');
-
 var _ = require('underscore');
 var MongoClient = require('mongodb').MongoClient;
 
 app.set('name', 'ATMGame');
+
+app.configure('production|development', function() {
+    app.loadConfig('mongoConfig', app.getBase() + '/../shared/config/mongoConfig.json');
+    app.loadConfig('code', app.getBase() + '/../shared/config/code.json');
+});
 
 // app configuration
 app.configure('production|development', 'connector', function() {
@@ -42,36 +45,35 @@ app.configure('production|development', 'chat', function() {
     );
 });
 
-
 // set modules global
 global.util = util;
 global._ = _;
 global.ATMGame = {};
 
-// read configs
-var mongoConfig = JSON.parse(fs.readFileSync('../shared/config/mongoConfig.json', 'utf8'));
-ATMGame.code    = JSON.parse(fs.readFileSync('../shared/config/code.json', 'utf8'));
+ATMGame.code = app.get('code');
 
-MongoClient.connect(mongoConfig.ATMGame.db, function(err, db) {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    ATMGame.mongodb = db;
-
-    // load mongodb base dao
-    ATMGame.MongoBaseDAO = require('../shared/dao/MongoBaseDAO.js');
-
-    // load mongodb other dao
-    var UserDAO = require('../shared/dao/UserDAO.js');
-    ATMGame.UserDAO = new UserDAO();
-    appStart();
-});
-function appStart() {
-    // start app
-    app.start();
-}
+appStart(app.get('mongoConfig').ATMGame);
 
 process.on('uncaughtException', function (err) {
     console.error(' Caught exception: ' + err.stack);
 });
+
+function appStart(mongoConfig) {
+    var mongoAddress = 'mongodb://' + mongoConfig.host + ':' + mongoConfig.port + '/' + mongoConfig.database;
+    MongoClient.connect(mongoAddress, function(err, db) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        ATMGame.mongodb = db;
+
+        // load mongodb base dao
+        ATMGame.MongoBaseDAO = require('../shared/dao/MongoBaseDAO.js');
+
+        // load mongodb other dao
+        var UserDAO = require('../shared/dao/UserDAO.js');
+        ATMGame.UserDAO = new UserDAO();
+
+        app.start();
+    });
+}
